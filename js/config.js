e5f2e7a7922cd54fd1edf4be115005bcc627edba
tai.nguyen <ductai2982@gmail.com>
2025-03-09 00:47:33 +0700

    upload source code

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const eventName = "Chúc mừng ngày 8/3";
+const actionName = "ĐẬP HỘP";
+const giftImage = "./image/avatar.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
+const titleCard = "Linh";
+const contentCard =
+  "Chúc Linh ngày 8/3 tràn ngập niềm vui và những nụ cười, luôn xinh đẹp, thành công và tràn đầy năng lượng. Mong những điều đẹp nhất sẽ đến với Linh trong hôm nay và cả những ngày sau.";
